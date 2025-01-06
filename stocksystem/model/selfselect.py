558461aadccd167cd6ec8ec41b825e2f8811e9da
kikyo464 <1650570950@qq.com>
2025-01-06 17:30:44 +0800

    对数据库的问题进行修正

--- stocksystem/model/selfselect.py
+++ stocksystem/model/selfselect.py
@@ -4,8 +4,8 @@ class SelfSelect(db.Model):
     __tablename__ = 'selfselect'
     __table_args__ = {'extend_existing': True}
     # 组合主键，确保每个用户只能选择每只股票一次
-    stockid = db.Column(db.Integer, db.ForeignKey('stock.stockid'), primary_key=True)  # 外键，关联股票表
-    userid = db.Column(db.Integer, db.ForeignKey('user.userid'), primary_key=True)    # 外键，关联用户表
+    stockid = db.Column(db.Integer, primary_key=True)  # 外键，关联股票表
+    userid = db.Column(db.Integer, primary_key=True)    # 外键，关联用户表
 
 
     def __repr__(self,userid, stockid):

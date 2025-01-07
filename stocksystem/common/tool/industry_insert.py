0edf06322410f7b28d11897c1f1ceca373edd67a
kikyo464 <1650570950@qq.com>
2025-01-07 09:41:44 +0800

    对股票表和行业表的添加方式更新

--- stocksystem/common/tool/industry_insert.py
+++ stocksystem/common/tool/industry_insert.py
@@ -1,28 +1,42 @@
 import pymysql
+import pandas as pd
 
 # 数据库连接配置
-conn = pymysql.connect(host='localhost', user='root', password='947400', database='stocksystem', charset='utf8mb4')
+conn = pymysql.connect(host='localhost', user='shixun', password='123456', database='stocksystem', charset='utf8mb4')
 cursor = conn.cursor()
 
 # 读取 CSV 文件
-import pandas as pd
-
-# 文件路径
-df = pd.read_csv("D:\\learning\\寒假实训\\shixun\\stocksystem\\news_data\\板块数据-3-utf8.csv")
+df = pd.read_csv("D:\\pycharmProject\\shixun\\stocksystem\\news_data\\个股数据.csv")
 
 # 清洗数据，移除包含nan值的行
-df_cleaned = df.dropna(subset=['行业'])
+df_cleaned = df.dropna(subset=['所属行业'])
 
-# 遍历每个行业名，插入前检查是否存在
-for industry in df_cleaned['行业']:
+# 遍历每个行业名，插入前检查是否存在，并获取 industryid
+for index, row in df_cleaned.iterrows():
+    industry = row['所属行业']
+    stockcode = row['股票代码']
+    stockname = row['股票名称']
+
     # 检查行业名是否已经存在
-    cursor.execute("SELECT COUNT(*) FROM industry WHERE industryname = %s", (industry,))
+    cursor.execute("SELECT industryid FROM industry WHERE industryname = %s", (industry,))
     result = cursor.fetchone()
 
-    # 如果不存在，则插入该行业名
-    if result[0] == 0:
+    # 如果行业名不存在，插入该行业，并获取 industryid
+    if result is None:
         cursor.execute("INSERT INTO industry (industryname) VALUES (%s)", (industry,))
-        conn.commit()
+        conn.commit()  # 提交插入操作
+        cursor.execute("SELECT industryid FROM industry WHERE industryname = %s", (industry,))
+        result = cursor.fetchone()  # 获取插入后的 industryid
+
+    # 获取 industryid
+    industryid = result[0]
+
+    # 插入股票数据到 stock 表
+    cursor.execute("""
+        INSERT INTO stock (stockcode, stockname, industryid) 
+        VALUES (%s, %s, %s)
+        """, (stockcode, stockname, industryid))
+    conn.commit()  # 提交插入操作
 
 # 关闭数据库连接
 cursor.close()
